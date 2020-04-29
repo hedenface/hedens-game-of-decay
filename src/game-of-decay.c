80d417fbe5e088670b782a7469127d076276b996
Bryan Heden <b.heden@gmail.com>
2020-04-28 21:36:09 -0500

    start breaking all this logic out

--- src/game-of-decay.c
+++ src/game-of-decay.c
@@ -32,12 +32,6 @@
 #define RIGHT (col + 1)
 
 
-int rows        = DEFAULT_ROWS;
-int cols        = DEFAULT_COLS;
-int range_min   = 0;
-int range_max   = 1;
-
-
 #if defined(TRUE) && TRUE != 1
     #undef TRUE
 #endif
@@ -53,6 +47,23 @@ int range_max   = 1;
 #endif
 
 
+typedef struct game_options {
+
+    int rows;
+    int cols;
+
+    char * range_string;
+    int range_min;
+    int range_max;
+
+    char ** map;
+    char * map_string;
+
+    int max_generations;
+
+} game_options;
+
+
 int print_version()
 {
     printf("%s\n", "Hedens Game of Decay - version 0.0.0\n");
@@ -63,12 +74,12 @@ int print_help()
 {
     print_version();
     printf("%s\n", "--------------------");
-    printf("%s\n", " ");
+    printf("%s", "\n");
     printf("%s\n", "-= based on Conways Game of Life =-");
-    printf("%s\n", " ");
-    printf("%s\n", " ");
+    printf("%s", "\n");
+    printf("%s", "\n");
     printf("%s\n", "Options:");
-    printf("%s\n", " ");
+    printf("%s", "\n");
     printf("%s\n", "-h,--help         Print this help and exit");
     printf("%s\n", "-v,--version      Print the current version and exit");
     printf("%s\n", "-r,--rows         Specify how many rows of cells to iterate over");
@@ -80,8 +91,8 @@ int print_help()
     printf("%s\n", "                  default: \"0= ,1=1,2=2,3=3,...Y=Y\"");
     printf("%s\n", "-g,--generations  Specify maximum number of generations to print");
     printf("%s\n", "                  minimum: 1");
-    printf("%s\n", " ");
-    printf("%s\n", " ");
+    printf("%s", "\n");
+    printf("%s", "\n");
 }
 
 
@@ -113,10 +124,152 @@ int parse_range(char * range, int * min, int * max)
 }
 
 
-int main (int argc, char **argv)
+int parse_arguments(int argc, char **argv, game_options * opts)
 {
     char c = 0;
     int option_index = 0;
+
+    static struct option long_options[] = {
+        { "help",        no_argument,       0, 'h' },
+        { "version",     no_argument,       0, 'v' },
+        { "rows",        optional_argument, 0, 'r' },
+        { "cols",        optional_argument, 0, 'c' },
+        { "range",       optional_argument, 0, 'R' },
+        { "map",         optional_argument, 0, 'm' },
+        { "generations", optional_argument, 0, 'g' },
+    };
+
+    while (TRUE) {
+
+        c = getopt_long(argc, argv, "hvr:c:R:m:g:", long_options, &option_index);
+
+        if (c == -1) {
+            break;
+        }
+
+        switch (c) {
+
+        case 'h':
+            return print_help();
+            break;
+
+        case 'v':
+            return print_version();
+            break;
+
+        case 'r':
+            opts->rows = atoi(optarg);
+            break;
+
+        case 'c':
+            opts->cols = atoi(optarg);
+            break;
+
+        case 'R':
+            opts->range_string = strdup(optarg);
+            //result = parse_range(optarg, &range_min, &range_max);
+            break;
+
+        case 'm':
+            opts->map_string = strdup(optarg);
+            break;
+
+        case 'g':
+            opts->max_generations = atoi(optarg);
+            break;
+        }
+    }
+}
+
+
+int set_default_map(game_options * opts)
+{
+
+}
+
+
+int parse_map_string(game_options * opts)
+{
+    if (opts->map_string == NULL) {
+        return TRUE;
+    }
+
+    // now lets parse the map_string and get our matches setup
+    ptr = strtok(map_string, "; ,");
+    while (ptr != NULL) {
+
+        i = 0;
+
+        char key[max_default_map_entry_len];
+        int map_key = 0;
+        char value[MAX_TEMP_BUFFER] = { 0 };
+        size_t value_len = 0;
+
+        char * token = strdup(ptr);
+        char * free_token = token;
+        char c = 0;
+        int processing_key = TRUE;
+
+        // probably a much more efficient way to do this
+        // i just didn't want to interrupt strtok on the potentially
+        // long string
+        while (c = *token++) {
+            if (processing_key == TRUE) {
+                if (c >= '0' && c <= '9') {
+                    key[i] = c;
+                }
+                else if (c == '=') {
+                    processing_key = FALSE;
+                    key[i] = 0;
+
+                    // just set it to -1 so it ends up at 0 on the
+                    // next pass for value[i]
+                    i = -1;
+                }
+            }
+            else {
+                value[i] = c;
+            }
+            i++;
+        }
+
+        value_len = strlen(value);
+
+        if (value_len > max_map_entry_len) {
+            max_map_entry_len = value_len;
+        }
+
+        map_key = atoi(key);
+
+        // first make sure the map_key is in appropriate range
+        if (map_key >= 0 && map_key <= range_max) {
+
+            // we can just overwrite the current map[key] hopefully..
+            if (value_len > max_default_map_entry_len) {
+                free(map[map_key]);
+                map[map_key] = calloc(value_len + 1, sizeof(char));
+            }
+
+            // update the value
+            snprintf(map[map_key], value_len + 1, "%s", value);
+        }
+
+        ptr = strtok(NULL, "; ,");
+        free(free_token);
+    }
+
+    free(map_string);
+}
+
+
+int sanity_check(game_options * opts)
+{
+
+}
+
+
+int main (int argc, char **argv)
+{
     int result = 0;
     int i = 0;
     int j = 0;
@@ -137,106 +290,10 @@ int main (int argc, char **argv)
     char * ptr = NULL;
 
     int generations = 0;
-    int max_generations = 0;
 
-    static struct option long_options[] = {
-        { "help",        no_argument,       0, 'h' },
-        { "version",     no_argument,       0, 'v' },
-
-        { "rows",        optional_argument, 0, 'r' },
-        { "cols",        optional_argument, 0, 'c' },
-
-        { "range",       optional_argument, 0, 'R' },
-
-        { "map",         optional_argument, 0, 'm' },
-
-        { "generations", optional_argument, 0, 'g' },
-    };
 
     srand(time(NULL));
 
-    opterr = 0;
-    while (TRUE) {
-
-        c = getopt_long(argc, argv, "hvr:c:R:m:g:", long_options, &option_index);
-
-        if (c == -1) {
-            break;
-        }
-
-        switch (c) {
-
-        case 'h':
-
-            if (map_string != NULL) {
-                free(map_string);
-            }
-
-            return print_help();
-            break;
-
-        case 'v':
-
-            if (map_string != NULL) {
-                free(map_string);
-            }
-
-            return print_version();
-            break;
-
-        case 'r':
-            rows = atoi(optarg);
-
-            if (rows <= 0) {
-                if (map_string != NULL) {
-                    free(map_string);
-                }
-                return print_help();
-            }
-
-            break;
-
-        case 'c':
-            cols = atoi(optarg);
-
-            if (cols <= 0) {
-                if (map_string != NULL) {
-                    free(map_string);
-                }
-                return print_help();
-            }
-
-            break;
-
-        case 'R':
-            result = parse_range(optarg, &range_min, &range_max);
-
-            if (result == FALSE) {
-                if (map_string != NULL) {
-                    free(map_string);
-                }
-                return print_help();
-            }
-
-            break;
-
-        case 'm':
-            map_string = strdup(optarg);
-            break;
-
-        case 'g':
-            max_generations = atoi(optarg);
-
-            if (max_generations < 1) {
-                if (map_string != NULL) {
-                    free(map_string);
-                }
-                return print_help();
-            }
-
-            break;
-        }
-    }
 
     // calculate final map
     map = calloc(range_max + 1, sizeof(* map));
@@ -248,75 +305,6 @@ int main (int argc, char **argv)
     for (i = 0; i <= range_max; i++) {
         map[i] = calloc(max_default_map_entry_len + 1, sizeof(char));
         snprintf(map[i], max_default_map_entry_len + 1, "%d", i);
-    }
-
-    if (map_string != NULL) {
-
-        // now lets parse the map_string and get our matches setup
-        ptr = strtok(map_string, "; ,");
-        while (ptr != NULL) {
-
-            i = 0;
-
-            char key[max_default_map_entry_len];
-            int map_key = 0;
-            char value[MAX_TEMP_BUFFER] = { 0 };
-            size_t value_len = 0;
-
-            char * token = strdup(ptr);
-            char * free_token = token;
-            char c = 0;
-            int processing_key = TRUE;
-
-            // probably a much more efficient way to do this
-            // i just didn't want to interrupt strtok on the potentially
-            // long string
-            while (c = *token++) {
-                if (processing_key == TRUE) {
-                    if (c >= '0' && c <= '9') {
-                        key[i] = c;
-                    }
-                    else if (c == '=') {
-                        processing_key = FALSE;
-                        key[i] = 0;
-
-                        // just set it to -1 so it ends up at 0 on the
-                        // next pass for value[i]
-                        i = -1;
-                    }
-                }
-                else {
-                    value[i] = c;
-                }
-                i++;
-            }
-
-            value_len = strlen(value);
-
-            if (value_len > max_map_entry_len) {
-                max_map_entry_len = value_len;
-            }
-
-            map_key = atoi(key);
-
-            // first make sure the map_key is in appropriate range
-            if (map_key >= 0 && map_key <= range_max) {
-
-                // we can just overwrite the current map[key] hopefully..
-                if (value_len > max_default_map_entry_len) {
-                    free(map[map_key]);
-                    map[map_key] = calloc(value_len + 1, sizeof(char));
-                }
-
-                // update the value
-                snprintf(map[map_key], value_len + 1, "%s", value);
-            }
-
-            ptr = strtok(NULL, "; ,");
-            free(free_token);
-        }
-
-        free(map_string);
     }
 
     // thanks c99
